5ed8872ae21f0cbf46ad12422dd6fec16e091534
vishu535 <vishveshwar.vishu@gmail.com>
2021-06-28 15:55:31 +0530

    added aggregate bat file

--- aggre.bat
+++ aggre.bat
@@ -1,2 +1,1 @@
-cd C:\Users\vishu\Documents\djj-2021\D365FO.Implementation.IntegrationTests
 mvn serenity:aggregate
---
+++ out/reports/navigator/precache-manifest.f127e49271f2b76cbdd844300c707680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e050777f40886d4e4c365324bb06d715",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61b968ce1c64656e2ab8",
+    "url": "./static/css/main.5ab818d3.chunk.css"
+  },
+  {
+    "revision": "ec4a2c77852e001dde8e",
+    "url": "./static/js/2.cae90c51.chunk.js"
+  },
+  {
+    "revision": "f1228651ed12f837338c31fb55903ecf",
+    "url": "./static/js/2.cae90c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b968ce1c64656e2ab8",
+    "url": "./static/js/main.564ef671.chunk.js"
+  },
+  {
+    "revision": "14224328b3d1963ed1ef",
+    "url": "./static/js/runtime-main.7b065930.js"
+  }
+]);
